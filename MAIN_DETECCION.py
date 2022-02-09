e00ae5764dba93bf8b60d009dd583d5d12e83faf
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2022-02-09 18:30:51 -0500

    Update MAIN_DETECCION.py

--- MAIN_DETECCION.py
+++ MAIN_DETECCION.py
@@ -4,172 +4,188 @@ import cv2
 import numpy as np
 from Localizador_ob import *
 import time
-import pyautogui
+import pyscreenshot
+import Graph as gp
 
-ventana = Tk()
+class Grafico_muestra:
 
-def abrirVideo():
-    seguimiento = Localizador_ob()
-    video = filedialog.askopenfilename(title="ABRIR VIDEO PARA DETECCIÓN")  # aqui se guarda el path del video
-    print(video)
-    lecturaVideo = cv2.VideoCapture(video)
+    def _init_(self, idvideo, idusuario, idvias, multa):
+        self.idVideo= idvideo
+        self.idUsuario = idusuario
+        self.idVias = idvias
+        self.multa = multa
 
-    #fps = 30
-    #fps = lecturaVideo.get(cv2.CAP_PROP_FPS)
-    #delay = 1 / fps
-    deteccion = cv2.createBackgroundSubtractorMOG2(history=10000,
-                                                   varThreshold=15)  # OPEN CV llama un metodo de segmentacion, nuestro objeto queda en negro y lo demas en blanco, historu se refiere al numero de procesamientos, y el umbral mas detecciones se tendra,(depende en donde vayamos a poner los videos o imageenes)
+        ventana = Tk()
+        Button(ventana, text="Abrir video a detectar", command=self.abrirVideo).pack()
+        ventana.mainloop()
 
-    carI = {}
-    car0 = {}
-    velocidades={}
 
-    while (lecturaVideo.isOpened()):
-        ret, vi = lecturaVideo.read()
+    def abrirVideo(self):
+        seguimiento = Localizador_ob()
+        self.grafic = gp.Graph(self.idVideo, self.idUsuario, self.idVias, self.multa)
+        video = filedialog.askopenfilename(title="ABRIR VIDEO PARA DETECCIÓN")  # aqui se guarda el path del video
+        print(video)
+        lecturaVideo = cv2.VideoCapture(video)
 
-        alto2 = vi.shape[0]  # (m, n) matriz 2
-        ancho2 = vi.shape[1]
+        # fps = 30
+        # fps = lecturaVideo.get(cv2.CAP_PROP_FPS)
+        delay = 2
+        deteccion = cv2.createBackgroundSubtractorMOG2(history=10000,
+                                                       varThreshold=15)  # OPEN CV llama un metodo de segmentacion, nuestro objeto queda en negro y lo demas en blanco, historu se refiere al numero de procesamientos, y el umbral mas detecciones se tendra,(depende en donde vayamos a poner los videos o imageenes)
 
-        mascara2 = np.zeros((alto2, ancho2), dtype=np.uint8)  # zeros rellenar matriz
+        carI = {}
+        car0 = {}
 
-        puntos = np.array([[[746, 293], [980, 293], [1142, 792], [476, 792]]])  # 2
+        velocidades = {}
+        aux = 0
+        while (lecturaVideo.isOpened()):
+            ret, vi = lecturaVideo.read()
 
-        cv2.fillPoly(mascara2, puntos, 255)  # 2
+            alto2 = vi.shape[0]  # (m, n) matriz 2
+            ancho2 = vi.shape[1]
 
-        zona = cv2.bitwise_and(vi, vi, mask=mascara2)  # 2
+            mascara2 = np.zeros((alto2, ancho2), dtype=np.uint8)  # zeros rellenar matriz
 
-        area_grande = [(746, 293), (980, 293), (1142, 792), (476, 792)]
-        area_1 = [(1142, 792), (476, 792), (590, 590), (1070, 590)]
-        area_2 = [(1070, 590), (590, 590), (675, 430), (1022, 430)]
-        area_3 = [(746, 293), (980, 293), (1022, 430), (675, 430)]
+            puntos = np.array([[[746, 293], [980, 293], [1142, 792], [476, 792]]])  # 2
 
-        cv2.polylines(vi, [np.array(area_grande, np.int32)], True, (255, 255, 255), 2)
+            cv2.fillPoly(mascara2, puntos, 255)  # 2
 
-        cv2.polylines(vi, [np.array(area_3, np.int32)], True, (255, 255, 255), 2)
+            zona = cv2.bitwise_and(vi, vi, mask=mascara2)  # 2
 
-        cv2.polylines(vi, [np.array(area_2, np.int32)], True, (255, 255, 255), 2)
+            area_grande = [(746, 293), (980, 293), (1142, 792), (476, 792)]
+            area_1 = [(1142, 792), (476, 792), (590, 590), (1070, 590)]
+            area_2 = [(1070, 590), (590, 590), (675, 430), (1022, 430)]
+            area_3 = [(746, 293), (980, 293), (1022, 430), (675, 430)]
 
-        cv2.polylines(vi, [np.array(area_1, np.int32)], True, (255, 255, 255), 2)
+            cv2.polylines(vi, [np.array(area_grande, np.int32)], True, (255, 255, 255), 2)
 
-        # if ret == False:
-        # break
-        # vi = cv2.resize(vi, (1920, 1080))  # se le baja la calidad al video
+            cv2.polylines(vi, [np.array(area_3, np.int32)], True, (255, 255, 255), 2)
 
-        # puros filtros que pasan una imagen mas limpia
-        mascara = deteccion.apply(zona)
-        # hace un blur
-        filtro = cv2.GaussianBlur(mascara, (19, 19), 0)
-        # este es el mas importante porque nos bota las cosas a blanco y negro
-        _, umbral = cv2.threshold(filtro, 127, 255, cv2.THRESH_BINARY)
+            cv2.polylines(vi, [np.array(area_2, np.int32)], True, (255, 255, 255), 2)
 
-        dilatacion = cv2.dilate(umbral, np.ones((5, 5)))
+            cv2.polylines(vi, [np.array(area_1, np.int32)], True, (255, 255, 255), 2)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))  # matrices de ceros y unos, convoluciones
+            # if ret == False:
+            # break
+            # vi = cv2.resize(vi, (1920, 1080))  # se le baja la calidad al video
 
-        cerrar = cv2.morphologyEx(dilatacion, cv2.MORPH_CLOSE, kernel)
+            # puros filtros que pasan una imagen mas limpia
+            mascara = deteccion.apply(zona)
+            # hace un blur
+            filtro = cv2.GaussianBlur(mascara, (19, 19), 0)
+            # este es el mas importante porque nos bota las cosas a blanco y negro
+            _, umbral = cv2.threshold(filtro, 127, 255, cv2.THRESH_BINARY)
 
-        # se esta aplicando a la zona de interes, es decir mps esta enviando la imagen negra
-        # _, mascara = cv2.threshold(mascara, 254, 255, cv2.THRESH_BINARY) #¡¡¡¡¡¡
-        contornos, _ = cv2.findContours(cerrar, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        detecciones = []
+            dilatacion = cv2.dilate(umbral, np.ones((5, 5)))
 
-        for i_contorno in contornos:
-            area_objeto = cv2.contourArea(i_contorno)
-            if area_objeto > 3000:  # se refiere a pixeles, esta sigue limpiando la imagen, hay puntitos blancos diminutos
-                x, y, ancho, alto = cv2.boundingRect(i_contorno)
-                # cv2.rectangle(zona, (x, y), (x + ancho, y + alto), (255, 255, 0), 3)
-                detecciones.append([x, y, ancho, alto])
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))  # matrices de ceros y unos, convoluciones
 
-        informacion_objeto_detectado = seguimiento.localizar(detecciones)
-        for i_informacion in informacion_objeto_detectado:
-            x, y, ancho, alto, id = i_informacion
-            # cv2.putText(zona, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
-            
-            vel = 0
-            infractor=True
-            screenshot=pyautogui.screenshot()
-            if vel == 0:
-                color = 0, 255, 255
-            cv2.rectangle(vi, (x, y), (x + ancho, y + alto), (color), 2)
+            cerrar = cv2.morphologyEx(dilatacion, cv2.MORPH_CLOSE, kernel)
 
-            punto_centralx = int(x + ancho / 2)
-            punto_centraly = int(y + alto / 2)
+            # se esta aplicando a la zona de interes, es decir mps esta enviando la imagen negra
+            # _, mascara = cv2.threshold(mascara, 254, 255, cv2.THRESH_BINARY) #¡¡¡¡¡¡
+            contornos, _ = cv2.findContours(cerrar, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            detecciones = []
 
-            seccion2 = cv2.pointPolygonTest(np.array(area_2, np.int32), (punto_centralx, punto_centraly), False)
+            for i_contorno in contornos:
+                area_objeto = cv2.contourArea(i_contorno)
+                if area_objeto > 3000:  # se refiere a pixeles, esta sigue limpiando la imagen, hay puntitos blancos diminutos
+                    x, y, ancho, alto = cv2.boundingRect(i_contorno)
+                    # cv2.rectangle(zona, (x, y), (x + ancho, y + alto), (255, 255, 0), 3)
+                    detecciones.append([x, y, ancho, alto])
 
-            if seccion2 >= 0:
-                carI[id] = time.process_time()
+            informacion_objeto_detectado = seguimiento.localizar(detecciones)
+            for i_informacion in informacion_objeto_detectado:
+                x, y, ancho, alto, id = i_informacion
+                # cv2.putText(zona, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
 
-            if id in carI:
+                vel = 0
+                infractor = True
 
-                cv2.circle(vi, (punto_centralx, punto_centraly), 3, (0, 0, 255), -1)
+                if vel == 0:
+                    color = 0, 255, 255
+                cv2.rectangle(vi, (x, y), (x + ancho, y + alto), (color), 2)
 
-                seccion3 = cv2.pointPolygonTest(np.array(area_3, np.int32), (punto_centralx, punto_centraly), False)
+                punto_centralx = int(x + ancho / 2)
+                punto_centraly = int(y + alto / 2)
 
-                if seccion3 >= 0:
+                seccion2 = cv2.pointPolygonTest(np.array(area_2, np.int32), (punto_centralx, punto_centraly), False)
 
-                    tiempo = time.process_time() - carI[id]
-                    if tiempo % 1 == 0:
-                        tiempo = tiempo + 0.3234
-                        #print(tiempo)
+                if seccion2 >= 0:
+                    carI[id] = time.process_time()
 
-                    if tiempo % 1 != 0:
-                        tiempo = tiempo + 1.016
-                        #print(tiempo)
+                if id in carI:
 
-                    if id not in car0:
-                        car0[id] = tiempo
+                    cv2.circle(vi, (punto_centralx, punto_centraly), 3, (0, 0, 255), -1)
 
-                    if id in car0:
-                        tiempo = car0[id]
+                    seccion3 = cv2.pointPolygonTest(np.array(area_3, np.int32), (punto_centralx, punto_centraly), False)
 
-                        vel = 24.5 / car0[id]
-                        vel *= 3.6
-                        if vel > 70:
-                            color = 0, 0, 255
-                           
-                            
+                    if seccion3 >= 0:
+
+                        tiempo = time.process_time() - carI[id]
+                        if tiempo % 1 == 0:
+                            tiempo = tiempo + 0.3234
+                            # print(tiempo)
+
+                        if tiempo % 1 != 0:
+                            tiempo = tiempo + 1.016
+                            # print(tiempo)
+
+                        if id not in car0:
+                            car0[id] = tiempo
+
+                        if id in car0:
+                            tiempo = car0[id]
+
+                            vel = 24.5 / car0[id]
+                            vel *= 3.6
+                            if vel > 60:
+                                captura_pantalla = True
+                                color = 0, 0, 255
+
+
+                            else:
+                                infractor = False
+                                color = 0, 255, 0
+                            cv2.rectangle(vi, (x, y), (x + ancho, y + alto), (color), 2)
+
+                        if id in velocidades:
+                            if velocidades[id][3] == 0:
+                                imagen = pyscreenshot.grab()
+                                imagen.save(f"Infractor_{str(id)}.png")
+                                velocidades[id][3] = f"Infractor_{str(id)}.png"
+                                #SEe pasan los datos de la gráfica
+                                self.grafic.guardarCarros(grafica)
+
                         else:
-                            infractor=False
-                            color = 0, 255, 0
-                            screenshot = pyautogui.screenshot()
-                            # Guardar imagen.
-                            pathScreenshot = f"Videos\\video-{id}.png"
-                            screenshot.save(pathScreenshot)
-                            
-                           
-                        cv2.rectangle(vi, (x, y), (x + ancho, y + alto), (color), 2)
-                    
-                    if not id in velocidades:
-                        grafica=[id,vel,infractor]
-                        velocidades[id]=grafica
-                        print(grafica)
-                    
-                    cv2.rectangle(vi, (x, y - 10), (x + 100, y - 50), (0, 0, 0), -1)
-                    cv2.putText(vi, str(int(vel)) + " KM / H", (x, y - 35), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
-                    #print(id)
-                    #print(vel)
+                            #bbox = (x, y, x + ancho, y + alto)
+                            grafica = [id, vel, infractor, 0]
+                            velocidades[id] = grafica
 
-                cv2.putText(vi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
-       
-        # print(informacion_objeto_detectado)
-        # cv2.imshow("Zona de interes", zona)
-        # cv2.imshow("calle", filtro)
-        
+                        cv2.rectangle(vi, (x, y - 10), (x + 100, y - 50), (0, 0, 0), -1)
+                        cv2.putText(vi, str(int(vel)) + " KM / H", (x, y - 35), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
+                        # print(id)
+                        # print(vel)
 
-        
-        cv2.imshow("calle", vi)
-        #cv2.imshow("cerrar", cerrar)
-        # cv2.imshow("zona donde se hara la detección", zona)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
+                    cv2.putText(vi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
 
-        #time.sleep(delay)
+            # print(informacion_objeto_detectado)
+            # cv2.imshow("Zona de interes", zona)
+            # cv2.imshow("calle", filtro)
 
-    lecturaVideo.release()
-    cv2.destroyAllWindows()
+            cv2.imshow("calle", vi)
+            #cv2.imshow("cerrar", cerrar)
+            # cv2.imshow("zona donde se hara la detección", zona)
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
 
+            # time.sleep(delay)
 
-Button(ventana, text="Abrir video a detectar", command=abrirVideo).pack()
+        lecturaVideo.release()
+        cv2.destroyAllWindows()
 
-ventana.mainloop()
+    def envento_boton(self):
+        self.grafic.graficarYMostrar()
+        self.grafic = gp.Graph(self.grafic)
+
+Grafico_muestra(1, 2, 3 ,4)
